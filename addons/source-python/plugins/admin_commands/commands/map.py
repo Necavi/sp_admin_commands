4c7754c744bcb882b313d32ad49cee8e8d1d5c29
necavi <necavi@0xf.org>
2015-05-19 18:35:03 -0700

    Added sp_utils submodule Updated all commands to use the c=(<colour>) syntax from sp_utils.colourizer

--- addons/source-python/plugins/admin_commands/commands/map.py
+++ addons/source-python/plugins/admin_commands/commands/map.py
@@ -1,7 +1,7 @@
 from engines.server import engine_server
 from commands import CommandReturn
 from listeners.tick.delays import tick_delays
-from .utils import Command
+from .sp_utils.utils import Command
 
 
 def change_map(level):
@@ -10,11 +10,11 @@ def change_map(level):
 @Command("sp_map", permission="sp.map.map")
 def sp_map(source, command):
     if command.get_arg_count() == 1:
-        source.message("Usage: ${purple}sp_map ${white}<map>")
+        source.message("c=(white)[c=(purple)SPc=(white)] Usage: $c=(purple)sp_map $c=(white)<map>")
         return CommandReturn.BLOCK
     level = command[1]
     if not engine_server.is_map_valid(level):
-        source.message("Map not found")
+        source.message("c=(white)[c=(purple)SPc=(white)] Map not found")
         return CommandReturn.BLOCK
     tick_delays.delay(3.0, change_map, level)
-    source.message("Changing map to {} in 3 seconds".format(level))
+    source.message("c=(white)[c=(purple)SPc=(white)] Changing map to {} in 3 seconds".format(level))
--- addons/source-python/plugins/admin_commands/commands/player.py
+++ addons/source-python/plugins/admin_commands/commands/player.py
@@ -1,15 +1,15 @@
 from commands import CommandReturn
-from .utils import Command, target_filter
+from .sp_utils.utils import Command, target_filter
 
 
 @Command("sp_rename", permission="sp.player.rename")
 def sp_rename(source, command):
     if command.get_arg_count() < 3:
-        source.message("Usage: ${purple}sp_rename${white}<name|#userid> <name>")
+        source.message("c=(white)[c=(purple)SPc=(white)] Usage: $c=(purple)sp_rename$c=(white)<name|#userid> <name>")
         return CommandReturn.BLOCK
     target = target_filter(command[1], source.index, False)
     if target is None:
-        source.message("Target not found.")
+        source.message("c=(white)[c=(purple)SPc=(white)] Target not found.")
         return CommandReturn.BLOCK
     name = command[2]
-    source.message("Rename is not implemented for this game.")
+    source.message("c=(white)[c=(purple)SPc=(white)] Rename is not implemented for this game.")
